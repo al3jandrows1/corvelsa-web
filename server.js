de308510979d079cdac75342a74968935747fbf4
alejandrows1 <alejandrovelasquezacm@gmail.com>
2026-05-24 17:26:34 -0600

    fix: force IPv4 DNS resolution globally

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
