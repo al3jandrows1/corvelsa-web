0dee96357505d771c744467bc15164ef214af9a5
alejandrows1 <alejandrovelasquezacm@gmail.com>
2026-07-11 08:52:29 -0600

    fix: use dynamic PORT for Render deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { Resend } = require('resend');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 app.use(express.json());
